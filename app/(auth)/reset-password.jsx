0016470322b9938bd047d29eba65053d0ec1327d
MoizRao <im.moiz616@gmail.com>
2024-10-06 17:51:03 +0500

    Added states and props

--- app/(auth)/reset-password.jsx
+++ app/(auth)/reset-password.jsx
@@ -1,13 +1,22 @@
-import { View, Text, ScrollView } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, Alert } from "react-native";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import InputField from "../../components/InputField";
 import CustomButton from "../../components/CustomButton";
 import { Link, router } from "expo-router";
 
 const ResetPassword = () => {
+    let [form, setForm] = useState({
+        npassword:'',
+        cpassword : ''
+    });
+
     const clickReset = () =>{
-        router.push("/sign-in")
+        if (form.npassword !== form.cpassword) {
+            Alert.alert("Error", "Passwords do not match");
+        } else {
+            router.push('/sign-in');
+        }
     }
 
     return (
@@ -20,14 +29,18 @@ const ResetPassword = () => {
                 <View className ="px-8">
                   <Text className="text-Text font-PoppinsSemiBold text-3xl">Reset Password</Text>
                     <InputField 
-                    title="New Password"
-                    value=""
-                    containerStyles={"mt-7"}
+                        title="New Password"
+                        placeholder="********"
+                        value={form.npassword}
+                        handleChangeText = {(e) => setForm({...form, npassword: e})}
+                        containerStyles={"mt-7"}
                     />
                     <InputField 
-                    title="Confirm Password"
-                    value=""
-                    containerStyles={"mt-7"}
+                        title="Confirm Password"
+                        placeholder="********"
+                        value={form.cpassword}
+                        handleChangeText = {(e) => setForm({...form, cpassword: e})}
+                        containerStyles={"mt-7"}
                     />
                     <CustomButton 
                     title="Reset"
--- components/InputField.jsx
+++ components/InputField.jsx
@@ -1,8 +1,11 @@
-import { StyleSheet, Text, View, TextInput } from 'react-native'
-import React from 'react'
- 
+import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import eye from '../assets/icons/eye.png';
+import eyeHide from '../assets/icons/eye-hide.png';
 
-const InputField = ({title, value, placeHolder, handleChangeText, containerStyles}) => {
+
+const InputField = ({title, value, placeHolder, handleChangeText, containerStyles, confirmPasswordProp}) => {
+  const [showPassword, setShowPassword] = useState(false)
   return (
     <View className={`space-y-2 ${containerStyles}`}>
       <Text className="text-xl text-Text font-PoppinsLight">{title}</Text>
@@ -11,16 +14,26 @@ const InputField = ({title, value, placeHolder, handleChangeText, containerStyle
         <TextInput
             className="flex-1 text-white font-PoppinsRegular text-base "
             value={value}
-            placeHolder={placeHolder}
+            placeholder={placeHolder}
             placeholderTextColor ="#7b7b8b"
             onChangeText = {handleChangeText}
-            // secureTextEntry={title === 'Password' && !showPassword}
+            secureTextEntry={(title === 'Password' || title === 'Confirm Password' || title === 'New Password') && !showPassword}
         />
+        {(title === 'Password' || title === 'Confirm Password' || title === 'New Password') && (
+            <TouchableOpacity onPress = {() => 
+                setShowPassword(!showPassword)
+            }>
+
+                <Image 
+                    source={!showPassword ? eye : eyeHide}
+                    className="w-6 h-6 "
+                    resizeMode='contain'
+                />
+            </TouchableOpacity>
+        )}
       </View>
     </View>
   )
 }
 
 export default InputField
-
-const styles = StyleSheet.create({})
