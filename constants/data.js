dafdc204b4eedc7f09f4825e5d2d3701d6307539
MoizRao <im.moiz616@gmail.com>
2024-11-24 19:28:14 +0500

    update location formatting

--- constants/data.js
+++ constants/data.js
@@ -1,25 +1,24 @@
-import { images } from "../constants";
+import { images } from "..,constants";
 
 export const locations = [
-  "Karachi/Pakistan",
-  "Lahore/Pakistan",
-  "Islamabad/Pakistan",
-  "Rawalpindi/Pakistan",
-  "Peshawar/Pakistan",
-  "Quetta/Pakistan",
-  "Multan/Pakistan",
-  "Faisalabad/Pakistan",
-  "Hyderabad/Pakistan",
-  "Sialkot/Pakistan",
-  "Gujranwala/Pakistan",
-  "Abbottabad/Pakistan",
-  "Sukkur/Pakistan",
-  "Bahawalpur/Pakistan",
-  "Mardan/Pakistan",
-  "Gujrat/Pakistan",
-  "Chiniot/Pakistan",
-  "Mingora/Pakistan",
-  "Jhelum/Pakistan",
-  "Sargodha/Pakistan"
+  "Karachi,Pakistan",
+  "Lahore,Pakistan",
+  "Islamabad,Pakistan",
+  "Rawalpindi,Pakistan",
+  "Peshawar,Pakistan",
+  "Quetta,Pakistan",
+  "Multan,Pakistan",
+  "Faisalabad,Pakistan",
+  "Hyderabad,Pakistan",
+  "Sialkot,Pakistan",
+  "Gujranwala,Pakistan",
+  "Abbottabad,Pakistan",
+  "Sukkur,Pakistan",
+  "Bahawalpur,Pakistan",
+  "Mardan,Pakistan",
+  "Gujrat,Pakistan",
+  "Chiniot,Pakistan",
+  "Mingora,Pakistan",
+  "Jhelum,Pakistan",
+  "Sargodha,Pakistan"
 ];
-
